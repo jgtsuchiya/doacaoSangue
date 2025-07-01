d713dabe54bfddbd067a571b2c54891c15591c53
jgbtsuchiya <joaog.tsuchiya@gmail.com>
2025-07-01 20:31:50 -0300

    Configurações

---
+++ src/main/java/com/example/sangue/SangueApplication.java
@@ -0,0 +1,13 @@
+package com.example.sangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SangueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SangueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sangue/SangueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SangueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
